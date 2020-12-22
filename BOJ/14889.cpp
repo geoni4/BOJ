a1b6940b44704868167755d0bba01db9057189d0
geoni4 <seralove4@gmail.com>
2020-12-22 23:35:43 +0900

    level 13 backtracking

--- BOJ/14889.cpp
+++ BOJ/14889.cpp
@@ -1,15 +1,57 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <iostream>
-
 using namespace std;
 
-int main(int argc, char** argv) {
-	int test_case;
-	freopen("14889.txt", "r", stdin);
-	cin >> test_case;
-	for (int tc = 1; tc <= test_case; ++tc) {
+#define MIN(A, B) (A) < (B) ? (A) : (B)
 
+int n, ans = 9876543210;
+int map[20][20];
+bool visited[20];
+
+int abs(int a, int b) {
+	if (a > b) return a - b;
+	return b - a;
+}
+
+void combi(int cnt, int cur) {
+	if (cnt == n / 2) {
+		int a = 0;
+		int b = 0;
+		for (int i = 0; i < n-1; i++) {
+			for(int j =i+1;j<n;j++){
+				if (visited[i] && visited[j]) {
+					a += map[i][j] + map[j][i];
+				}
+				else if (!visited[i] && !visited[j]) {
+					b += map[i][j] + map[j][i];
+				}
+			}
+		}
+		ans = MIN(ans, abs(a - b));
+		return;
 	}
+
+	for (int i = cur; i < n; i++) {
+		if (!visited[i] && i > cur) {
+			visited[i] = 1;
+			if (ans == 0) return;
+			combi(cnt + 1, i);
+			visited[i] = 0;
+		}
+	}
+}
+
+
+int main() {
+	freopen("14889.txt", "r", stdin);
+	cin >> n;
+	for (int y = 0; y < n; y++) {
+		for (int x = 0; x < n; x++) {
+			cin >> map[y][x];
+		}
+	}
+	combi(0, 0);
+	cout << ans;
 	return 0;
 }
---
+++ BOJ/9372.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int tc;
+	cin.tie(0), ios::sync_with_stdio(0);
+	cin >> tc;
+
+	for (int t = 0; t < tc; t++) {
+		int n, m;
+		cin >> n >> m;
+		for (int i = 0; i < m; i++) {
+			int k;
+			cin >> k >> k;
+		}
+		cout << n - 1 << '\n';
+	}
+
+	return 0;
+}
