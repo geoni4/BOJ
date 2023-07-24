2f2ba70d60e6a415a7ebadb002100484d229d665
tatelulove4 <tatelulove4@naver.com>
2023-07-24 15:16:36 +0900

    commit and push

---
+++ BOJ/sources/2/7000/27433.cpp
@@ -0,0 +1,61 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+
+using namespace std;
+
+long long factorial(long long n) {
+	if (n <= 1) return 1;
+	return n*factorial(n - 1);
+}
+
+
+int main() {
+	freopen("./resources/2/7000/27433.txt", "r", stdin);
+	long long n;
+	cin >> n;
+	cout << factorial(n);
+	return 0;
+}
+
+
+/*
+
+https://www.acmicpc.net/problem/27433
+
+팩토리얼 2
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+1 초 	1024 MB	10661	5116	4684	50.102%
+문제
+
+0보다 크거나 같은 정수 N이 주어진다. 이때, N!을 출력하는 프로그램을 작성하시오.
+입력
+
+첫째 줄에 정수 N(0 ≤ N ≤ 20)이 주어진다.
+출력
+
+첫째 줄에 N!을 출력한다.
+예제 입력 1
+
+10
+
+예제 출력 1
+
+3628800
+
+예제 입력 2
+
+0
+
+예제 출력 2
+
+1
+
+알고리즘 분류
+
+	수학
+	사칙연산
+
+메모
+
+*/
